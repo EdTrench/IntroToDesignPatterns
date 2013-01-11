703b791cba38d7e67c820de87bc9d0242b41297a
Edward G Trenchard <ed_trench@yahoo.com>
2013-01-11 15:00:14 +0000

    11-01-2013

---
+++ IntroToDesignPatterns/Duck.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IntroToDesignPatterns
+{
+    abstract class Duck
+    {
+        public IFlyBehavior flyBehavior;
+        public IQuackBehavior quackBehavior;
+
+        public Duck()
+        {
+
+        }
+
+        public abstract void display();
+
+        public void performFly()
+        {
+            flyBehavior.fly();
+        }
+
+        public void performQuack()
+        {
+            quackBehavior.quack();
+        }
+
+        public void swim()
+        {
+            Console.WriteLine("All ducks float, event decoys!");
+        }
+    }
+}
---
+++ IntroToDesignPatterns/FlyNoWay.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IntroToDesignPatterns
+{
+    class FlyNoWay : IFlyBehavior
+    {
+        public void fly()
+        {
+            Console.WriteLine("I can't fly");
+        }
+    }
+}
